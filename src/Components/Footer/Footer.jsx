168eff47a3aec20cc06bbf25d3e16685ad4c2500
shahin4317 <shahinalam4317@gmail.com>
2026-04-28 15:30:40 +0600

    fixed responsive problem

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -4,10 +4,10 @@ import React from 'react';
 const Footer = () => {
     return (
         <div className='bg-gray-900 text-white text-center'>
-            
+
 
             <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-8 px-4 sm:px-6 md:px-10 lg:px-16 py-10 '>
-                
+
                 {/* Logo */}
                 <div className='space-y-5'>
                     <h2 className='text-2xl md:text-3xl font-bold'>DigiTools</h2>
@@ -26,7 +26,6 @@ const Footer = () => {
                     <p className='text-sm text-gray-400'>Integrations</p>
                 </div>
 
-
                 <div className='space-y-3'>
                     <p className='text-lg font-semibold'>Link</p>
                     <p className='text-sm text-gray-400'>Blog</p>
@@ -34,7 +33,7 @@ const Footer = () => {
                     <p className='text-sm text-gray-400'>Press</p>
                 </div>
 
-   
+
                 <div className='space-y-3'>
                     <p className='text-lg font-semibold'>Resources</p>
                     <p className='text-sm text-gray-400'>Documentation</p>
@@ -45,11 +44,40 @@ const Footer = () => {
 
                 <div className='space-y-3'>
                     <p className='text-lg font-semibold'>Social Links</p>
-                    <div className='flex gap-3'>
-                       
+                    <div className='flex gap-3 sm:ml-30 lg:ml-20'>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="24"
+                            height="24"
+                            viewBox="0 0 24 24"
+                            className="fill-current">
+                            <path
+                                d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path>
+                        </svg>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="24"
+                            height="24"
+                            viewBox="0 0 24 24"
+                            className="fill-current">
+                            <path
+                                d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path>
+                        </svg>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="24"
+                            height="24"
+                            viewBox="0 0 24 24"
+                            className="fill-current">
+                            <path
+                                d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
+                        </svg>
+
                     </div>
                 </div>
             </div>
+         
+            <br />
 
             <div className='flex flex-col sm:flex-row justify-between items-center text-sm text-gray-400 px-4 sm:px-6 md:px-10 lg:px-16 pb-6 gap-3'>
                 <p>© 2026 Digitools. All rights reserved.</p>
--- src/Components/Rating/Rating.jsx
+++ src/Components/Rating/Rating.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Rating = () => {
     return (
-        <div className='flex flex-col sm:flex-row justify-center items-center bg-blue-600 gap-6 sm:gap-10 md:gap-20 px-4 py-10 sm:py-12 md:py-16 mb-10 text-center'>
+        <div className='flex flex-col sm:flex-row justify-center items-center bg-blue-500 gap-6 sm:gap-10 md:gap-20 px-4 py-10 sm:py-12 md:py-16 mb-10 text-center'>
             
             <div>
                 <h3 className='text-2xl sm:text-3xl md:text-4xl font-bold text-white'>50K+</h3>
@@ -16,7 +16,7 @@ const Rating = () => {
 
             <div>
                 <h3 className='text-2xl sm:text-3xl md:text-4xl font-bold text-white'>4.9</h3>
-                <p className='text-sm text-white'>Rating</p>
+                <p className='text-sm text-white'>Rating </p>
             </div>
 
         </div>
